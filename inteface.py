979559990d37b0aa61390913318503ac2310d74b
Brayan Ulloa <brayaneillo2.7182@unimilitar.edu.co>
2022-09-09 11:18:29 -0500

    Actualización 9/09

--- inteface.py
+++ inteface.py
@@ -11,7 +11,7 @@ import serial, serial.tools.list_ports
 from tkinter import ttk
 
 #Configuracion COM
-board =serial.Serial(port='COM1', baudrate=9600)
+board =serial.Serial(port='COM1', baudrate=19200)
 #board.write( b'fine\r\n' )
 sleep(5) #5 Segundos Para Que Establezca La Comunicacion
 
@@ -197,23 +197,23 @@ def close():
 
 #Envio de datos por boton
 def show_values1():
+    board.write("Rojo".encode()) 
+    # #Cuadro_Texto_1
+    # board.write(b'Eb,')
+    # board.write(txt_edit_ang0.get(1.0, tk.END).encode())    
 
-    #Cuadro_Texto_1
-    board.write(b'Eb,')
-    board.write(txt_edit_ang0.get(1.0, tk.END).encode())    
+    # #Cuadro_Texto_2
+    # board.write(b'Ebr,')
+    # board.write(txt_edit_ang1.get(1.0, tk.END).encode())
 
-    #Cuadro_Texto_2
-    board.write(b'Ebr,')
-    board.write(txt_edit_ang1.get(1.0, tk.END).encode())
+    # #Cuadro_Texto_3 
+    # board.write(b'Eab,')
+    # board.write(txt_edit_ang2.get(1.0, tk.END).encode())
 
-    #Cuadro_Texto_3 
-    board.write(b'Eab,')
-    board.write(txt_edit_ang2.get(1.0, tk.END).encode())
-
-    #Cuadro_Texto_4
-    board.write(b'Em,')
-    board.write(txt_edit_ang3.get(1.0, tk.END).encode())
-    dato2(2)
+    # #Cuadro_Texto_4
+    # board.write(b'Em,')
+    # board.write(txt_edit_ang3.get(1.0, tk.END).encode())
+    # dato2(2)
 
 def show_values2():
     
